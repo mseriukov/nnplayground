f25a3bb4f5a55a719b90337facb9d1eb55e0e1a8
Mikhail Seriukov <mseriukov@gmail.com>
2023-08-10 22:03:53 +0300

    Initial commit.

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "nnPlayground",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "nnPlayground",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "cnnutils"
+            ],
+            path: "Sources/nnPlayground"
+        ),
+        .target(
+            name: "cnnutils",
+            path: "Sources/cnnutils",
+            publicHeadersPath: "include"
+        )
+    ]
+)
