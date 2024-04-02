968a3dcfc1b71abdb0622ea2111a6a87b7b2399b
Mikhail Seriukov <mseriukov@gmail.com>
2024-04-02 21:55:46 +0300

    WIP: tensor broadcasting.

--- Sources/AlgebraKit/Tensor.swift
+++ Sources/AlgebraKit/Tensor.swift
@@ -8,6 +8,12 @@ import Accelerate
 
 
 public struct Tensor {
+
+    public enum Errors: Error {
+        case shapeIsZeroLength
+        case broadcastIsntPossible
+    }
+
     public private(set) var storage: [Float]
     public private(set) var shape: [Int]
     public private(set) var strides: [Int] = []
@@ -16,11 +22,52 @@ public struct Tensor {
         assert(data.count == shape.reduce(1, *), "Input data doesn't match provided tensor shape.")
         self.shape = shape
         self.storage = data
+        reshape(shape)
+    }
 
-        strides.append(1)
+    mutating func reshape(_ shape: [Int]) {
+        guard shape.reduce(1, *) == storage.count else { fatalError("New shape doesn't fit the data.") }
+        strides = [1]
         for shapeEntry in shape.dropFirst().reversed() {
             strides.insert(strides.first! * shapeEntry, at: 0)
         }
+    }
 
+    func element(at indicies: [Int]) -> Float {
+        guard indicies.count == shape.count else { fatalError("indicies vs shape mismatch") }
+        var offset: Int = 0
+        for i in 0..<shape.count {
+            offset += shape[i] * indicies[i]
+        }
+        return storage[offset]
+    }
+
+    private func broadcast(_ shape1: [Int], _ shape2: [Int]) throws -> [Int] {
+        guard shape1.count > 0, shape2.count > 0 else {
+            throw Errors.shapeIsZeroLength
+        }
+        let N1 = shape1.count
+        let N2 = shape2.count
+        let N = max(N1, N2)
+
+        var result = Array(repeating: 1, count: N)
+
+        for i in (0..<N).reversed() {
+            let n1 = N1 - N + i
+            let n2 = N2 - N + i
+            let d1 = n1 >= 0 ? shape1[n1] : 1
+            let d2 = n2 >= 0 ? shape2[n2] : 1
+
+            if d1 == 1 {
+                result[i] = d2
+            } else if d2 == 1 {
+                result[i] = d1
+            } else if d1 == d2 {
+                result[i] = d1
+            } else {
+                throw Errors.broadcastIsntPossible
+            }
+        }
+        return result
     }
 }
