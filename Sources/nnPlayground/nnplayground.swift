ac0284ed7e598062e4b008060d86067ee24870c4
Mikhail Seriukov <mseriukov@gmail.com>
2024-12-24 19:25:21 +0200

    Make it to not suspend the app.

--- Sources/nnPlayground/nnplayground.swift
+++ Sources/nnPlayground/nnplayground.swift
@@ -24,32 +24,42 @@ struct nnplayground: ParsableCommand {
         Monitor.shared.run { monitor in
             let display1 = Monitor.shared.addDisplay(title: "Test1", size: .zero)
             let display2 = Monitor.shared.addDisplay(title: "Test2", size: .zero)
-            DispatchQueue.global(qos: .userInitiated).async {
-                do {
-                    try MLPTests().run(url: inputURL, testURL: testURL) { image in
-                        DispatchQueue.main.async {
-                            guard let image else { return }
-                            display1.setImage(image)
-                            display2.setImage(image)
-                        }
-                    }
-                } catch {}
 
-//                guard let image = NSImage(data: MachOSectionReader.getEmbeddedData("cats_gs")) else { return }
-//                DispatchQueue.main.async {
-//                    display1.setImage(image)
-//                    var imageMatrix = image.asMatrix()[0]
-//                    var filter = Matrix.laplacian5x5
-//                    let fsize = filter.size.cols
-//                    imageMatrix = im2col(imageMatrix, fsize)
-//                    filter.reshape(size: Size(1, filter.size.elementCount))
-//                    var r = filter * imageMatrix
-//                    r.reshape(size: Size(512 - fsize + 1))
-//                    r.normalize()
-//                    r.mapInPlace { $0 = 1.0 / (1.0 + exp(-$0)) }
-//                    let resultImage = ImageBuilder.buildImage(from: r.padded(25, value: 0), colorTransform: { Viridis.color($0) })
-//                    display2.setImage(resultImage)
-//                }
+            let activity = ProcessInfo.processInfo.beginActivity(
+                options: .userInitiated,
+                reason: "Having some fun here."
+            )
+            let customQueue = DispatchQueue(
+                label: "com.nnpalyground.workQueue",
+                qos: .userInitiated
+            )
+            customQueue.async {
+//                do {
+//                    try MLPTests().run(url: inputURL, testURL: testURL) { image in
+//                        DispatchQueue.main.async {
+//                            guard let image else { return }
+//                            display1.setImage(image)
+//                            display2.setImage(image)
+//                        }
+//                    }
+//                } catch {}
+
+                guard let image = NSImage(data: MachOSectionReader.getEmbeddedData("cats_gs")) else { return }
+                DispatchQueue.main.async {
+                    display1.setImage(image)
+                    var imageMatrix = image.asMatrix()[0]
+                    var filter = Matrix.laplacian5x5
+                    let fsize = filter.size.cols
+                    imageMatrix = im2col(imageMatrix, fsize)
+                    filter.reshape(size: Size(1, filter.size.elementCount))
+                    var r = filter * imageMatrix
+                    r.reshape(size: Size(512 - fsize + 1))
+                    r.normalize()
+                    r.mapInPlace { $0 = 1.0 / (1.0 + exp(-$0)) }
+                    let resultImage = ImageBuilder.buildImage(from: r.padded(25, value: 0), colorTransform: { Viridis.color($0) })
+                    display2.setImage(resultImage)
+                }
+                ProcessInfo.processInfo.endActivity(activity)
             }
         }
     }
